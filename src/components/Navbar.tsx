869a27e09ec2fb11a55028bd631caf67376287f4
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-09 14:41:29 -0800

    コンポーネント追加、firebase追加、fontawesome追加

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import "./styles/Navbar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faFilePen, faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons'
+
+
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link to={'/'}><FontAwesomeIcon icon={faHouse} />ホーム</Link>
+      <Link to={'/createpost'}><FontAwesomeIcon icon={faFilePen} />記事投稿</Link>
+      <Link to={'/login'}><FontAwesomeIcon icon={faArrowRightToBracket} />ログイン</Link>
+    </nav>
+  )
+}
+
+export default Navbar
