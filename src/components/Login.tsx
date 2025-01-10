582fcffbcf1a5109795d30d2e6fe48b08817032a
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-10 10:15:57 -0800

    login logout追加

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,26 @@
+import { signInWithPopup } from "firebase/auth"
+import { auth, provider } from "../firebase"
+import { useNavigate } from "react-router-dom";
 
+interface LoginProps {
+  setIsAuth: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-const Login = () => {
+const Login: React.FC<LoginProps> = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const loginInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem('isAuth', 'true');
+      setIsAuth(true);
+      navigate('/');
+    });
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoogle}>Googleでログイン</button>
+    </div>
   )
 }
 
--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,8 +1,26 @@
+import { signInWithPopup, signOut } from "firebase/auth"
+import { auth, provider } from "../firebase"
+import { useNavigate } from "react-router-dom";
 
+interface LoginProps {
+  setIsAuth: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-const Logout = () => {
+const Logout: React.FC<LoginProps> = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const logout = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      navigate('/login');
+    });
+  }
+
   return (
-    <div>Logout</div>
+    <div>
+      <p>ログアウトする</p>
+      <button onClick={logout}>ログアウト</button>
+    </div>
   )
 }
 
