757662df775abd163be7c8d473ef158549c1e2c0
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-11 16:13:30 -0800

    ホームコンポーネントHTML作成

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,20 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+      </div>
+      <div className="postTextContainer">
+        a;lsjfklasjglkjdal;kdjgl;akjsldkjaflkdjklafjlksdjflkasjlkdgoihqeoigjhlakjfdlksjalfjd
+      </div>
+      <div className="nameAndDeleteButton">
+        <h3>@keiazdev</h3>
+        <button>削除</button>
+      </div>
+    </div>
   )
 }
 
